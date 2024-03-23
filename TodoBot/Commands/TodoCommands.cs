a432558355d907255e49ce74d1a3c5b3e94361cb
Elianora Rose <developer@elianorarose.com>
2024-03-22 23:42:07 -0400

    Adding ability to set custom description

--- TodoBot/Commands/TodoCommands.cs
+++ TodoBot/Commands/TodoCommands.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus;
 using DSharpPlus.SlashCommands;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 using TodoBot.Entities;
 
 namespace TodoBot.Commands;
@@ -8,32 +9,41 @@ namespace TodoBot.Commands;
 public class TodoCommands(IDbContextFactory<TodoDbContext> dbFactory) : ApplicationCommandModule
 {
     [SlashCommand("add-todo", "Adds a todo for the current user")]
-    public async Task AddTodoAsync(InteractionContext context)
+    public async Task AddTodoAsync(InteractionContext context, [Option("description", "Description for the todo")]string description)
     {
-        using var db = dbFactory.CreateDbContext();
-        db.Todos.Add(new Todo
+        using (var db = dbFactory.CreateDbContext())
         {
-            DiscordUserId = context.User.Id,
-            Description = "Test"
-        });
+            db.Todos.Add(new Todo
+            {
+                DiscordUserId = context.User.Id,
+                Description = description
+            });
 
-        db.SaveChanges();
+            db.SaveChanges();
+        }
 
         await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new()
         {
-            Content = "Response!"
+            Content = "Todo added!"
         });
     }
 
     [SlashCommand("get-todos", "Retrieves a list of todos for the current user")]
     public async Task GetTodosAsync(InteractionContext context)
     {
-        using var db = dbFactory.CreateDbContext();
-        var todos = db.Todos.Where(todo => todo.DiscordUserId == context.User.Id).ToList();
-        
+        var response = new StringBuilder();
+        using (var db = dbFactory.CreateDbContext())
+        {
+            var todos = db.Todos.Where(todo => todo.DiscordUserId == context.User.Id).ToList();
+            foreach (var todo in todos)
+            {
+                response.AppendLine(todo.Description);
+            }
+        }
+
         await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new()
         {
-            Content = "Response!"
+            Content = response.ToString()
         });
     }
 }
